a1e9e7728e1545b18e971396d09917183cbed205
ykanno0521 <54009743+ykanno0521@users.noreply.github.com>
2021-03-29 07:28:21 +0900

    styled-componentの設定

---
+++ app/javascript/components/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import { Swich, Route, Link } from 'react-router-dom'
+import styled from 'styled-compoments'
+import AddTodo from './AddTodo'
+import EditTodo from './EditTodo'
+import TodoList from './TodoList'
+import './App.css'
+
+
+const NavBar = styled.nav`
+  backGround: #dbfffe;
+  min-height:8vh;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`
+
+const Logo = styled.div`
+  font-weight:  bold;
+  font-size: 3px;
+  letter-spacing: 3px;
+`
+
+const NavItems = styled.ul`
+  display: flex;
+  width: 400px;
+  max-width: 40%;
+  justify-content: space-around;
+  list-style: none;
+`
+const NavItem = styled.li`
+  font-size: 19px;
+  font-weight: bold;
+  opacity: 0.7;
+  &:hover {
+    opacity: 1;
+  }
+`
+
+const Wrapper = styled.div`
+  width: 700px;
+  max-width: 85%;
+  margin: 20% auto;
+`
+
+function App() {
+  return (
+    <div>
+      App
+    </div>
+  )
+}
+
+export default App
